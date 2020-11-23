ac721c46b1263caa9716fa93d68a5df5eee8e1c0
Natalia Seguin <nataliaseguin@gmail.com>
2020-11-23 14:36:01 +0100

    corregido error

--- paquete/HolaMundo.java
+++ paquete/HolaMundo.java
@@ -1,9 +1,10 @@
-package paquete;
+
 public class HolaMundo {
 
-	public static void main(String[] args) {		
-		System.out.println("Hola Mundo");
-	}
+        public static void main(String[] args) {
+                System.out.println("Hola Mundo");
+        }
 
 }
+
 
